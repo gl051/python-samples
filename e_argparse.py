9539db7c6e9acddba68850783f7ef75b96596509
Gianluca <gl051@biccari.com>
2016-09-23 15:29:46 -0700

    Add example of using argparse

---
+++ e_argparse.py
@@ -0,0 +1,11 @@
+""" Demo-ing argparse. """
+
+import argparse
+parser = argparse.ArgumentParser(description='This is a sample program')
+parser.add_argument("user", help="user")
+parser.add_argument("age", help="age for the user", type=int)
+parser.add_argument("-v", "--verbose", help="verbose mode", action="store_true")
+args = parser.parse_args()
+print ('User {}, Age {}'.format(args.user, args.age))
+if args.verbose:
+    print ('Running in verbose mode')
