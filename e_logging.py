bcf3423af8ddd63518c8237a303075b8e26010c3
Gianluca <gl051@biccari.com>
2016-09-23 17:47:34 -0700

    Improve logging example

--- e_logging.py
+++ e_logging.py
@@ -1,14 +1,32 @@
 """ Demo logging."""
 
 import logging
-logger = logging.getLogger()
-handler = logging.StreamHandler()
+
+LOG_FILE = './output/test.log'
+
+# Get a logger class linked to an id/name
+logger = logging.getLogger('MyApp')
+
+# Define a formatter for the messages
 formatter = logging.Formatter('%(asctime)s - [%(levelname)8s]: %(message)s')
+
+# Create handler for streaming the log.
+# You can have more than one.
+
+handler = logging.StreamHandler()
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
+handler_file = logging.StreamHandler(open(LOG_FILE, 'w'))
+handler_file.setFormatter(formatter)
+logger.addHandler(handler_file)
+
+# Set minimum level logging
 logger.setLevel(logging.DEBUG)
-logger.debug('this is debig')
-logger.warning('this is warning')
-logger.info('this is info')
-logger.error('this is error')
-logger.critical('this is critical')
+
+# This is how to log stuff
+logger.info('this is an info message')
+logger.debug('this is a debug message')
+logger.warning('this is a warning message')
+logger.error('this is an error message')
+logger.critical('this is a critical message')
