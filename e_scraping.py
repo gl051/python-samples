d767fba16bac9ea8aaa3953e07f2c50de8c9705a
Gianluca <gl051@biccari.com>
2016-11-03 09:03:49 -0700

    Add user input for year pick

--- e_scraping.py
+++ e_scraping.py
@@ -1,20 +1,24 @@
+#!/usr/bin/env python
+
 import requests
 from bs4 import BeautifulSoup
 
+# website to scrap (movie database)
 URL_ROOT = "https://www.themoviedb.org/"
 
-# Most popoular movie
+# Find movie URL
 endpoint = URL_ROOT + "discover/movie"
 
-# payload
-payload = {'sort_by': 'popularity.desc', 'primary_release_year': '2011'}
-YEAR = 1945
-payload['primary_release_year'] = YEAR
+# payload, to provide parameters for the url
+payload = {'sort_by': 'popularity.desc', 'primary_release_year': '1945'}
+# You can dinamically build your query adding params to the payload
+year = input('Search most popoular movie in (year): ')
+payload['primary_release_year'] = year
 
 # use request to get the response from the API
 try:
     response = requests.get(endpoint, payload)
-
+    # check that we got a page back
     if response.status_code is not requests.codes.ok:
         print ('Error with this url {}'.format(r.url))
     else:
@@ -26,9 +30,9 @@ try:
         # get the title (form the tag value)
         titles = [tag.string for tag in tags]
         # print order by popularity
-        print ("YEAR: {}".format(YEAR))
+        print ("YEAR: {}".format(year))
         for i, t in enumerate(titles, start=1):
             print ("#{} {}".format(i, t))
 except Exception as ex:
-    print ('Ops!')
+    print ('Ops, something went wrong!')
     print (ex)
