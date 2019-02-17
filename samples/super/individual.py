f543a8d162ffe0baa810c40aa6126e002c650dd1
Gianluca <gl051@biccari.com>
2019-02-17 11:41:52 -0800

    Remove unused property

--- samples/super/individual.py
+++ samples/super/individual.py
@@ -7,7 +7,6 @@ class Individual(object):
         def __init__(self, firstname, lastname):
             self.firstname = firstname
             self.lastname = lastname
-            self.gender = 'N/A'
 
         def fullname(self):
             return f'{self.firstname} {self.lastname}'
